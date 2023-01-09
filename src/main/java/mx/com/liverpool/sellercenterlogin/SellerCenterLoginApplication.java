a7c9d0d7e4e9c99ae8b8b8fef205cd9ca28abba4
rlgarcial <ricardogl_23@hotmail.com>
2023-01-09 06:44:28 -0600

    init commit

---
+++ src/main/java/mx/com/liverpool/sellercenterlogin/SellerCenterLoginApplication.java
@@ -0,0 +1,13 @@
+package mx.com.liverpool.sellercenterlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerCenterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerCenterLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/liverpool/sellercenterlogin/SellerCenterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.liverpool.sellercenterlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerCenterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
